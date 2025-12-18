0d6d5db7373fd6005b08956b61216589ca919947
HemangDudhat <hemangdudhat@gmail.com>
2025-12-18 15:20:31 +0530

    Refactor code structure for improved readability and maintainability

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,16 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 
+export const metadata = {
+  title: "Deal Drop",
+  description: "",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        
+        {children}
       </body>
     </html>
   );
